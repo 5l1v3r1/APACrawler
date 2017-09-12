3c1150172b944074c03c76480f6c9ca963d1b600
Haoxi Tan <heman0426@gmail.com>
2017-09-12 18:25:13 +1200

    Now with a working interface and GET request handler.

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
-from flask import Flask
-import BeautifulSoup
+from flask import Flask,request
+from bs4 import BeautifulSoup
 
-app = Flask(__name__)
+app = Flask(__name__) #initiates Flask app
 
-@app.route('/')
-def hello_world():
-    page = '''
+
+#default page
+page = '''
 
     <title>APACrawler -- APA autocite</title>
 
+    <head>
+
+    </head>
+
     <header>
     <h1>APACrawler</h1>
-    <h2>Welcome to APACrawler, the APA auto citer 
-    powered by Python 3 and flask!</h2>
+    <p>Welcome to APACrawler, the APA auto citer 
+    powered by Python 3 and flask!</p>
+    </header>
 
+    <form method="GET" action="/cite">
+
+            <input type="text" name="url">
+
+            <input type="submit" value="Cite">
+
+    </form>
 
     '''
 
 
+@app.route('/') #route is the 'directory'
+def main():
+    #returns that page
     return page
+
+
+@app.route('/cite', methods=['GET'])
+def cite():
+    error = None
+    if request.method == 'GET':
+        url=request.args.get('url')
+        return page+url;
+    else:
+        return page
+
 
 if __name__ == '__main__':
     app.run()

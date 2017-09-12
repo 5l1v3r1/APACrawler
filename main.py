2e7fac8dbb9225974c1ad9e165e38b92d4f394ce
Haoxi Tan <heman0426@gmail.com>
2017-09-12 17:59:06 +1200

    Second commit, getting web config to work and modifying the example

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from flask import Flask
+import BeautifulSoup
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+    page = '''
+
+    <title>APACrawler -- APA autocite</title>
+
+    <header>
+    <h1>APACrawler</h1>
+    <h2>Welcome to APACrawler, the APA auto citer 
+    powered by Python 3 and flask!</h2>
+
+
+    '''
+
+
+    return page
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
